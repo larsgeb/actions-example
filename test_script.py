968cd7ff43e66cc1bcf5e4917d7b4a54f21dea95
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2020-06-22 15:22:50 +0200

    I added a test script

---
+++ test_script.py
@@ -0,0 +1,19 @@
+import numpy
+
+
+def test_matmul():
+
+    A = numpy.ones((5, 5))
+
+    B = numpy.diag(numpy.arange(5) + 1)
+
+    print()
+    print("Matrix A:")
+    print(A)
+    print("Matrix B:")
+    print(B)
+
+    result = A @ B
+
+    print(result)
+
